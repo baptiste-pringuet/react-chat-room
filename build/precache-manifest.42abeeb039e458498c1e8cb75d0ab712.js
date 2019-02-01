8fbaca11d3a604e88dffc3b88de29268498ceceb
Baptiste <pringuetb.bp@gmail.com>
2019-02-01 16:44:53 +0100

    add build

---
+++ build/precache-manifest.42abeeb039e458498c1e8cb75d0ab712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cdf746bf850434cda45a",
+    "url": "/static/js/main.cdf746bf.chunk.js"
+  },
+  {
+    "revision": "e9b62b2a93a361bc84dd",
+    "url": "/static/js/1.e9b62b2a.chunk.js"
+  },
+  {
+    "revision": "cdf746bf850434cda45a",
+    "url": "/static/css/main.3afdfb60.chunk.css"
+  },
+  {
+    "revision": "c878fed1edb05ee20bb64292431db991",
+    "url": "/index.html"
+  }
+];
